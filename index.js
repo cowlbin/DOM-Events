0ba8d63a09834075c1d729475bf6eb963d3c5e79
David Cano <dacano@msn.com>
2022-12-14 22:29:57 -0500

    updated index  .html/.js where buttons are functional to print to console but unable to change font size

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// document = ​​​​​​gitCode/DOM-Events/index.html
+// const body = document.querySelector("body");
+
+//Cookie Clicker - click cookie img to increase cookie click counter
+
 const cookieButton = document.querySelector("#cookieImg");
 let counter = 0;
 
@@ -6,5 +11,27 @@ cookieButton.addEventListener("click", function () {
   counter = counter + 1;
   const incCount = document.querySelector("#clickCounter");
   incCount.textContent = counter;
-  
+});
+
+//Magnifying buttons - increase/decrease font size
+const paraEle = document.querySelector("#magPara");
+
+const bigButton = document.querySelector("#incrFontSize");
+
+bigButton.textContent = "Increase font size";
+
+bigButton.addEventListener("click", function () {
+  console.log("The BIG button was pressed");
+  const fontSize = parseInt(paraEle.style.fontSize);
+  paraEle.style.fontSize = (fontSize + 1) + "px";
+});console.log(paraEle.style.fontSize)
+
+
+const smallButton = document.querySelector("#decrFontSize");
+smallButton.textContent = "Decrease font size";
+smallButton.addEventListener("click", function () {
+  console.log("The small button was pressed");
+
+  const fontSize = parseInt(paraEle.style.fontSize);
+  paraEle.style.fontSize = (fontSize - 1) + "px";
 });
